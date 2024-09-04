cbe23487922aebcfda69015bc21fefe56d33b03e
elisedare28 <125211920+elisedare28@users.noreply.github.com>
2024-09-05 02:33:35 +0530

    CAPTCHA appears after button is clicked

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,48 @@
 import { useState } from 'react';
 import './App.css'; 
 
-const Keypad = ({ onClick }) => {
-  const keys = [
-    '1', '2', '3',
-    '4', '5', '6',
-    '7', '8', '9','0'
-  ];
 
-  return (
-    <div className="keypad">
-      {keys.map((key) => (
-        <button 
-          key={key} 
-          className="keypad-button" 
-          onClick={() => onClick(key)}
-        >
-          {key}
-        </button>
-      ))}
-      <div className="random-number-box">
-        {Math.floor(Math.random() * 10)}
-      </div>
-    </div>
-  );
-};
 
 function App() {
   const [showKeypad, setShowKeypad] = useState(false);
   const [input, setInput] = useState('');
-
+  const Keypad = ({ onClick }) => {
+    const keys = [
+      '1', '2', '3',
+      '4', '5', '6',
+      '7', '8', '9','0'
+    ];
+  
+    return (
+      
+      <div className="keypad">
+        
+        {keys.map((key) => (
+          <button 
+            key={key} 
+            className="keypad-button" 
+            onClick={() => onClick(key)}
+          >
+            {key}
+          </button>
+        
+        ))}
+        <div className='generated-number'>
+              <label className="form-section__label">Enter the given number</label>
+                <input 
+                  name="num" 
+                  autoComplete="off" 
+                  className="form-section__input" 
+                  value={input}
+                  readOnly 
+                />
+          </div>
+        <div className="random-number-box">
+          {Math.floor(Math.random() * 10)}
+        </div>
+      </div>
+    );
+  };
   const handleKeyClick = (key) => {
     setInput(prevInput => prevInput + key);
   };
@@ -51,16 +64,7 @@ function App() {
                 className="form-section__input" 
               />
             </div>
-            <div className='generated-number'>
-            <label className="form-section__label">Enter the given number</label>
-              <input 
-                name="num" 
-                autoComplete="off" 
-                className="form-section__input" 
-                value={input}
-                readOnly 
-              />
-            </div>
+            
             <div className="form-section__button-container">
               <button 
                 type="button" 
